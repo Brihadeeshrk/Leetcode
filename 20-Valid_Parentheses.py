126364dc7ff00a7556bd293250192f953857d330
briha <brihadeeshrk@gmail.com>
2022-01-03 21:40:09 +0530

    202

---
+++ 20-Valid_Parentheses.py
@@ -0,0 +1,49 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+'''
+
+class Solution(object):
+    def isValid(self, s):
+         # Stack for left symbols
+        leftSymbols = []
+        # Loop for each character of the string
+        for c in s:
+            # If left symbol is encountered
+            if c in ['(', '{', '[']:
+                leftSymbols.append(c)
+            # If right symbol is encountered
+            elif c == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
+                leftSymbols.pop()
+            elif c == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
+                leftSymbols.pop()
+            elif c == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
+                leftSymbols.pop()
+            # If none of the valid symbols is encountered
+            else:
+                return False
+        return leftSymbols == []
