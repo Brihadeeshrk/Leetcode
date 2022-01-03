a997bc5b881308b891af74fb69f26147cc5321ee
briha <brihadeeshrk@gmail.com>
2022-01-03 21:40:44 +0530

    20

--- 20-Valid_Parentheses.py
+++ 20-Valid_Parentheses.py
@@ -5,12 +5,12 @@ An input string is valid if:
 
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
- 
 
 Example 1:
 
 Input: s = "()"
 Output: true
+
 Example 2:
 
 Input: s = "()[]{}"
